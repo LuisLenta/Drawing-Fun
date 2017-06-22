bd004bffa8d45835e0aa5628579137aa6e85ecf0
Luis Lenta <luislenta@gmail.com>
2017-06-22 20:29:30 -0300

    Primer parte

---
+++ Drawingfun/app/src/main/java/com/example/usuario/drawingfun/DrawingView.java
@@ -0,0 +1,20 @@
+package com.example.usuario.drawingfun;
+
+import android.content.Context;
+import android.view.View;
+import android.util.AttributeSet;
+
+
+/**
+ * Created by USUARIO on 22/06/2017.
+ */
+
+public class DrawingView extends View {
+    public DrawingView(Context context) {
+        super(context);
+        setupDrawing();
+    }
+
+    private void setupDrawing() {
+    }
+}
